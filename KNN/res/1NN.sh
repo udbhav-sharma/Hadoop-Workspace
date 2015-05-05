76284742014034d157ee618ca10d17c6ca445ac1
Udbhav <udbhav.sharma15@gmail.com>
2015-05-05 20:48:26 +0530

    Compilation scripts added

---
+++ KNN/res/1NN.sh
@@ -0,0 +1,4 @@
+hdfs dfs -rm /1NN/out/*
+hdfs dfs -rmdir /1NN/out
+
+hadoop jar /home/hduser/Workspace_jar/1NN.jar
---
+++ KNN/res/VD.sh
@@ -0,0 +1,10 @@
+hdfs dfs -rm /VD/out/*
+hdfs dfs -rmdir /VD/out
+
+hdfs dfs -rm /VD/out2/*
+hdfs dfs -rmdir /VD/out2
+
+hdfs dfs -rm /VD/out3/*
+hdfs dfs -rmdir /VD/out3
+
+hadoop jar /home/hduser/Workspace_jar/VD.jar
